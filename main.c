ae1d530a9099c307f56741f067aea6bd728a6c3f
Your Name <k.soto@pucp.pe>
2021-04-10 00:19:35 -0500

    changes of main to include new logical operators like || &&

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ * @ac: number of arguments
+ * @av: Array of arguments
+ *
+ * Return: 0 on success
+*/
+int main(int __attribute__((unused))ac, char **av)
+{
+	int read;
+	char *buff = NULL;
+	size_t buff_len = 0;
+
+	while (1)
+	{
+		/* Print console symbol only if it is interactive*/
+		if (isatty(STDIN_FILENO) == 1)
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+		/* Read commands from console */
+		read = getline(&buff, &buff_len, stdin);
+		/* Remove comments & '\n' char from buffer */
+		buff = handle_comment(buff);
+		_strtok(buff, "\n");
+		/* Handling_semicolon, ||, && and executes inside of the function */
+		handling_semicolon_and_operators(buff, read, av[0]);
+	}
+	/* Free buffer memory */
+	free(buff);
+	return (0);
+}
