46269b2d8a06672368d8d3eadc74043d72c6e3eb
kateincoding <kateincoding@gmail.com>
2021-04-11 17:17:35 +0000

    add strdup, strcmp, strncmp

--- f_strings.c
+++ f_strings.c
@@ -13,6 +13,7 @@ int _strlen(const char *s)
 		i++;
 	return (i);
 }
+
 /**
  * _strdup - copy a strings with a malloc
  * @s1: array
@@ -34,4 +35,34 @@ char *_strdup(const char *s1)
 	}
 	s2[i] = '\0';
 	return (s2);
+}
+
+/**
+ * _strcmp - compare if is equal 2 strings
+ * @s1: string1
+ * @s2: string2
+ * Return: 0 success; else pointer 
+ */
+int	_strcmp(const char *s1, const char *s2)
+{
+	return (_strncmp(s1, s2, (size_t)-1));
+}
+
+/**
+ * _strncmp - compare if is equal 2 strings, first n chars
+ * @s1: string1
+ * @s2: string2
+ * Return: 0 success; else pointer 
+ */
+int	_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n && s1 && s2 && s1[i] && s2[i] &&
+			(unsigned char)s1[i] == (unsigned char)s2[i])
+		i++;
+	if (i == n)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
