6f56d236428b0f9f73b221b2a4b00857117c92aa
renatoleonholberton <2829@holbertonschool.com>
2021-04-15 17:58:51 +0000

    Fix exit codes number

--- f_handle_shell_logical_operators.c
+++ f_handle_shell_logical_operators.c
@@ -105,24 +105,19 @@ int execute_commands(char *buff, char **cmds_list, char *cmd,
 		free_dbl_ptr(commands);
 		return (-1);
 	}
-	/* have a flag when the execution is an error (||)*/
-	if (handle_PATH(commands) == -1)
-	{
-		perror(first_av);
-		free_dbl_ptr(commands);
-		return (-1);
-	}
 	/* check if we can only run for positives */
 	child_pid = fork();/* Fork parent process to execute the command */
 	if (child_pid == -1)
 		dispatch_error(first_av);
 	else if (child_pid == 0)
-	{ /* execute command */
+	{
+		handle_PATH(commands);
 		execve(commands[0], commands, __environ);
 		free_allocs(buff, cmds_list, commands, F_BUFF | F_CMD_L | F_CMDS);
 		dispatch_error(first_av);
 	}
 	wait(status);
+	set_process_exit_code(*status / 256);
 	free_dbl_ptr(commands);
 	return (flag);
 }
