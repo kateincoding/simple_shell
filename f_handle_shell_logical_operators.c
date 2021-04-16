bda9545f345dce3ef82a46d5a7eb13e01a6d66d7
renatoleonholberton <2829@holbertonschool.com>
2021-04-16 16:08:35 +0000

    Remove PATH condition

--- f_handle_shell_logical_operators.c
+++ f_handle_shell_logical_operators.c
@@ -90,7 +90,7 @@ int execute_commands(char *buff, char **cmds_list,
 	char *cmd, int __attribute__((unused))read, char *first_av)
 {
 	char **commands;
-	int child_pid, _err = 0, flag = 0, *status = process_exit_code();
+	int child_pid, flag = 0, *status = process_exit_code();
 
 	/* Generate array of commands */
 	commands = parse_user_input(cmd, " ");
@@ -113,11 +113,8 @@ int execute_commands(char *buff, char **cmds_list,
 	}
 	else if (child_pid == 0)
 	{
-		_err = handle_PATH(commands);
-		if (_err == 0)
-			execve(commands[0], commands, __environ);
-		else
-			handle_cmd_not_found(buff, cmds_list, commands, first_av);
+		handle_PATH(commands);
+		execve(commands[0], commands, __environ);
 		free_allocs(buff, cmds_list, commands, F_BUFF | F_CMD_L | F_CMDS);
 		dispatch_error(first_av);
 	}
